9ff57cd286079a0c19ac421329e7e5e4f21d72e0
wun <kevinlay841009@gmail.com>
2024-04-17 10:08:31 +0800

    0417  第一節課

---
+++ 20240417/lowdb01/index.mjs
@@ -0,0 +1,58 @@
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+import { v4 as uuidv4 } from "uuid";
+
+const defaultData = {
+    user: {},
+    products: []
+};
+const db = new Low(new JSONFile("db.json"), defaultData);
+await db.read();
+
+// 讀取
+// let data = db.data.products.find(p => p.id === "46582a66-f4a3-4855-8bf4-1c1da8f7775d")
+// let data = db.data.products.find(p => p.title === "damn糕");
+// let data = db.data.products.filter(p => p.title.includes("damn"));
+
+let page = 1;
+let limit = 5;
+let start = (page - 1) * limit;
+let end = page * limit;
+// let data = db.data.products.slice(start, end);
+let data = db.data.products.sort((a, b) => { a.price - b.price }).slice(start, end);
+console.log(data);
+
+// console.log(db.data.user);
+
+
+// 寫入
+
+// db.data.products.push({
+//     id: uuidv4(),
+//     title: "damn糕",
+//     price: 45,
+//     stock: 100,
+//     createTime:Date.now()
+// });
+
+// await db.write();
+
+// db.data.products.unshift({
+//     id: uuidv4(),
+//     title: "后里蟹",
+//     price: 1500,
+//     stock: 1000,
+//     createTime: Date.now()
+// })
+// await db.write();
+
+// let uid = uuidv4();
+// let user = {
+//     id: uid,
+//     account: "bro",
+//     password: "a12345",
+//     name: "bro蟹",
+//     head: "http://randomuser.me/api/portraits/men/44.jpg"
+// };
+// db.data.user[uid] = user;
+// await db.write();
