d0df754ed6574a29fa54f861678bc3fe1ce9f1de
wun <kevinlay841009@gmail.com>
2024-04-22 11:02:12 +0800

    0422  第一+二節課

---
+++ 20240422/websocket03/server/index.mjs
@@ -0,0 +1,34 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const clients = {};
+
+wss.on("connection", (connection) => {
+    console.log("新的使用者已連線");
+
+    connection.on("message", (message) => {
+        console.log(`收到訊息 -> ${message}`);
+        const parsedMessage = JSON.parse(message);
+
+        if (parsedMessage.type === "register") {
+            const userID = parsedMessage.userID;
+            clients[userID] = connection;
+            return false;
+        }
+
+        if (parsedMessage.type === "message") {
+
+            return falsee;
+        }
+        // wss.clients.forEach((client) => {
+        //     if (client.readyState === WebSocket.OPEN) {
+        //         client.send(message);
+        //     }
+        // });
+    });
+
+    connection.on("close", () => {
+        console.log("使用者斷開連線");
+    })
+})
